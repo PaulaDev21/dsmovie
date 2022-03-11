4402466f5cc882b9c4bdf01294c630290bc4e789
Paula Lopes <pauladev21@gmail.com>
2022-03-11 19:20:22 -0300

    EvaluationForm created

--- frontend/src/pages/EvaluationForm/index.tsx
+++ frontend/src/pages/EvaluationForm/index.tsx
@@ -1,9 +1,41 @@
+import "./styles.css"
+
+const movie = {
+    id: 1,
+    image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+    title: "The Witcher",
+    count: 2,
+    score: 4.5
+};
+
 function EvaluationForm() {
     return (
-        <div>
-            <h1>Avaliação dos filmes</h1>
-            <a href="/">Voltar p/ listagem</a>
-        </div>
+        <div className="dsmovie-form-container">
+            <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
+            <div className="dsmovie-card-bottom-container">
+                <h3>{movie.title}</h3>
+                <form className="dsmovie-form">
+                    <div className="form-group dsmovie-form-group">
+                        <label htmlFor="email">Your Best Email</label>
+                        <input type="email" className="form-control" id="email" />
+                    </div>
+                    <div className="form-group dsmovie-form-group">
+                        <label htmlFor="score">How much did you like the movie?</label>
+                        <select className="form-control" id="score">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </div>
+                    <div className="dsmovie-form-btn-container">
+                        <button type="submit" className="btn btn-primary dsmovie-btn">Save Evaluation</button>
+                    </div>
+                </form >
+                <button className="btn btn-primary dsmovie-btn mt-3">Cancel</button>
+            </div >
+        </div >
     );
 };
 
