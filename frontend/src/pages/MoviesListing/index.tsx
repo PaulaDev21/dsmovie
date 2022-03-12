fc2b9c5c05bbf191c99e862a34179d081b793027
Paula Lopes <pauladev21@gmail.com>
2022-03-12 12:19:16 -0300

    Pagination component implemented and added to MoviesListing page

--- frontend/src/pages/MoviesListing/index.tsx
+++ frontend/src/pages/MoviesListing/index.tsx
@@ -1,7 +1,9 @@
+import Pagination from "components/Pagination";
 
 function MoviesListing() {
     return (
         <div>
+            <Pagination />
             <h1>Listagem do filmes</h1>
             <a href="EvaluationForm/1" > LINK DO FORM</a>
         </div>
